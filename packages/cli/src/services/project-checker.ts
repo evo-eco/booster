00da648f1bbe11a6aab1dc741609baf62d9a1279
David Verdú <davidverdu@theagilemonkeys.com>
2021-02-08 12:42:24 +0000

    start version comparing

--- packages/cli/src/services/project-checker.ts
+++ packages/cli/src/services/project-checker.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
+import { Logger } from '@boostercloud/framework-types'
 
 function checkIndexFileIsBooster(indexFilePath: string): void {
   const contents = fs.readFileSync(indexFilePath)
@@ -27,4 +28,33 @@ export async function checkItIsABoosterProject(projectPath: string): Promise<voi
       `There was an error when recognizing the application. Make sure you are in the root path of a Booster project:\n${e.message}`
     )
   }
+}
+
+export async function checkCurrentDirBoosterVersion(logger: Logger, userAgent: string): Promise<void> {
+  return checkBoosterVersion(logger, userAgent, process.cwd())
+}
+
+export async function checkBoosterVersion(logger: Logger, userAgent: string, projectPath: string): Promise<void> {
+  const projectVersion = await getBoosterVersion(projectPath)
+  const cliVersion = userAgent.split(' ')[0].split('/')[2]
+  await compareVersionsAndDisplayMessages(logger, cliVersion, projectVersion)
+}
+
+async function getBoosterVersion(projectPath: string): Promise<string> {
+  const projectAbsolutePath = path.resolve(projectPath)
+  try {
+    const packageJsonContents = require(path.join(projectAbsolutePath, 'package.json'))
+    const version = packageJsonContents.dependencies['@boostercloud/framework-core']
+    return version.replace('^','')
+  } catch (e) {
+    throw new Error(
+      `There was an error when recognizing the application. Make sure you are in the root path of a Booster project:\n${e.message}`
+    )
+  }
+}
+
+async function compareVersionsAndDisplayMessages(logger: Logger, cliVersion: string, projectVersion: string): Promise<void> {
+  if (cliVersion === projectVersion)  { return }
+  //TODO
+  logger.info("versions checked")
 }
